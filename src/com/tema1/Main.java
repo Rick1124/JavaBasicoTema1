216c2a09a71f4afb639852eff9ea2993f425c152
Rick1124 <38147073+Rick1124@users.noreply.github.com>
2022-08-18 00:16:07 -0500

    Tema 1 ejercicio

---
+++ src/com/tema1/Main.java
@@ -0,0 +1,22 @@
+package com.tema1;
+
+public class Main {
+    public static void main(String[] args) {
+        byte variable1 = 1;
+        short variable2 = 2;
+        int variable3 = 3;
+        long variable4 = 4;
+        float variable5 = 5;
+        double variable6 = 6;
+        char variable7 = 'a';
+        String variable8 = "Hola";
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println(variable7);
+        System.out.println(variable8);
+    }
+}
